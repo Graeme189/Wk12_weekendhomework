3899d6dfeddba6b836cf4ee9f25e288cd9673d67
Graeme Henderson <graeme.189@gmail.com>
2019-09-13 11:57:15 +0100

    Guitar class complete and tests passing

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,4 +1,17 @@
 package instruments;
 
-public class Guitar {
+import instruments.Instrument;
+
+public class Guitar extends Instrument {
+
+    private int numberOfStrings;
+
+    public Guitar(String colour, String model, String make, int numberOfStrings){
+        super(colour, model, make);
+        this.numberOfStrings = numberOfStrings;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
+    }
 }
---
+++ src/test/java/GuitarTest.java
@@ -0,0 +1,36 @@
+import instruments.Guitar;
+import instruments.Instrument;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("black", "Swamp Ash", "PRS", 6);
+    }
+
+    @Test
+    public void canGetColour() {
+        assertEquals("black", guitar.getColour());
+    }
+
+    @Test
+    public void canGetModel() {
+        assertEquals("Swamp Ash", guitar.getModel());
+    }
+
+    @Test
+    public void canGetMake() {
+        assertEquals("PRS", guitar.getMake());
+    }
+
+    @Test
+    public void canGetNumberOfStrings() {
+        assertEquals(6, guitar.getNumberOfStrings());
+    }
+}
